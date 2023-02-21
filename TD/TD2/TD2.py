84b83aca8b0e401a4a3644a3511ff22e59fa29dc
Aginthan SIVANESAN <aginthan.sivanesan@ens.uvsq.fr>
2023-02-21 10:57:52 +0100

    TD

--- TD/TD2/TD2.py
+++ TD/TD2/TD2.py
@@ -1,24 +1,24 @@
 msg = "hello world"
 print(msg.capitalize())
 
-from tkinter import *
+import tkinter as tk
 
 CANVAS_WIDTH, CANVAS_HEIGHT = 600, 400
 
-root = Tk()
+root = tk()
 
-canvas = Canvas(root, width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
+canvas = tk.Tk(root, width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
 
     # Début de votre code
 x0 = 100
 x1 = CANVAS_WIDTH - 100
 y = CANVAS_HEIGHT / 2
-canvas.create_line(x0, y, x1, y)
-canvas.create_oval(x0 - 50, y + 50, x0 + 50, y - 50)
-canvas.create_oval(x1 - 50, y + 50, x1 + 50, y - 50)
-canvas.create_oval((x0 + x1) / 2 - 50, y + 50, (x0 + x1) / 2 + 50, y - 50)
+tk.create_line(x0, y, x1, y)
+tk.create_oval(x0 - 50, y + 50, x0 + 50, y - 50)
+tk.create_oval(x1 - 50, y + 50, x1 + 50, y - 50)
+tk.create_oval((x0 + x1) / 2 - 50, y + 50, (x0 + x1) / 2 + 50, y - 50)
     
     # Fin de votre code
 
-canvas.grid()
+tk.grid()
 root.mainloop()
